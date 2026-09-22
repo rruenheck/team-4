ca57e17fc947971123b353e8d70460c7467cec43
ray <rruenheck@umass.edu>
2026-09-22 11:41:28 -0400

    added views folder and an account page

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const PORT = 3000;
+
+
+const accountSettings = {
+      "id" : 1,
+      "name" : "jane doe",
+      "number_of_pets" : 0,
+      "inventory" : {},
+    }
+
+app.get('/account',(req,res)=>{
+  res.render("account", { title: "Account Page", accountSettings});
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
